bea949d001167b67e7f4f1c7dc90e9422b26dc97
Fadhila Syahida Wibowo <fasyasyahida@gmail.com>
2024-08-28 07:57:03 +0700

    Add files via upload

---
+++ Dhila01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dhila01;
+
+/**
+ *
+ * @author hp
+ */
+public class Dhila01 {
+
+    public static void main(String[] args) {
+        String nama = "Fadhila Syahida Wibowo";
+        String npm = "2320506046";
+        String alamat = "Sragen";
+        String noHP = "0895344514856";
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM: " + npm);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("No Hp: " + noHP);
+    }
+}
